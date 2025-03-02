19113368e3bf2d5ee98e6e2b2d64732eace52ba7
kaleemullah <kaleemullahdev@gmail.com>
2025-03-02 20:58:35 +0500

    initial commit

--- send_email.php
+++ send_email.php
@@ -23,14 +23,13 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 } 
 
 
-$apiKey = 'api_021PT1DoslYvBYqaRDxCty.0WaOjA0mf5NnCqieRRnDHO';  // Replace with your actual API key
-// $leadId = 'lead_Zukn4LIuVNcX2Py3uPoVu3uRD6MnN2nAUX9hMcWedZZ'; // Replace with your lead ID
+$apiKey = 'yourkey';  // Replace with your actual API key
 
 
 
 $data = [
     "name" => $email,
-    "url" => "https://www.bitslap.io/",
+    "url" => "https://www.yourwebsite.com/",
     "description" => "",
     "contacts" => [
         [
@@ -74,79 +73,13 @@ if ($httpCode >= 200 && $httpCode < 300) {
 
     
     echo "Lead created successfully!\n";
-    // Optionally process the response data (e.g., decode JSON)
-//     $responseData = json_decode($response, true);
-//     echo "<pre>";
-//     print_r($responseData);  // Print the response data
-// die();
 
 } else {
 
     echo "Error : $response (HTTP code $httpCode)\n";
-    // Handle the error appropriately (e.g., log it, display a message to the user)
+ 
 
 }
-
-// if ($err) {
-//     echo "cURL Error #:" . $err;
-// } else {
-//     $resp = json_decode($response, true); // Decode JSON response
-//     print_r($resp);  // Print or process the response
-// }
-// die();
-
-
-
-
-// $data = [
-//     'lead_id' => $leadId,
-//     "name"=>explode('@',$email)[0],
-//     'emails' => [
-//         [
-//             'email' => $email, // The email address you want to send
-//             'type' => 'office'  // Or other type if appropriate
-//         ],        
-//         ]
-// ];
-
-
-
-// $jsonData = json_encode($data);
-
-// $ch = curl_init('https://api.close.com/api/v1/contact/');
-// curl_setopt_array($ch, [
-//     CURLOPT_USERPWD => "$apiKey:", // API key authentication
-//     CURLOPT_POST => true,
-//     CURLOPT_POSTFIELDS => $jsonData,
-//     CURLOPT_HTTPHEADER => [
-//         'Content-Type: application/json',
-//         'Accept: application/json'
-//     ],
-//     CURLOPT_RETURNTRANSFER => true // Return the response instead of outputting it
-// ]);
-
-// $response = curl_exec($ch);
-// $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);  // Get HTTP status code
-
-
-// if ($httpCode >= 200 && $httpCode < 300) {
-
-    
-//     echo "Contact created successfully!\n";
-//     // Optionally process the response data (e.g., decode JSON)
-//     $responseData = json_decode($response, true);
-// //     echo "<pre>";
-// //     print_r($responseData);  // Print the response data
-// // die();
-
-// } else {
-
-//     echo "Error creating/updating contact: $response (HTTP code $httpCode)\n";
-//     // Handle the error appropriately (e.g., log it, display a message to the user)
-
-// }
-
-
 
 ?>
 
